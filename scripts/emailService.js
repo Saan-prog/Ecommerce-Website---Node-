f65cdac34d7dff39cb8bba30393dcbfcbab58bfb
Saan-prog <ssandra6045@gmail.com>
2026-02-06 14:10:15 +0530

    New

--- scripts/emailService.js
+++ scripts/emailService.js
@@ -1,5 +1,6 @@
 const { Resend } = require("resend");
 
+require('dotenv').config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendPasswordResetEmail = async (email, resetLink) => {
